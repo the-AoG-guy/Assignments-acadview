9bd548d199c24126367b948ec955a92b96f12ae5
Amrit Dash <37613080+ddash123@users.noreply.github.com>
2018-04-24 13:11:38 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,17 @@
+# Question 1
+
+print "Hey!"
+print "The code is %&#!*"
+print "Please call 320288328 to verify."
+
+# Question 2
+
+print "I'd like a Martini please. Shaken, not stirred."
+
+# Question 3
+
+print "Hello World!"
+
+# Question 4
+
+print 'Mourinho says,"Manchester united are used to losing in UCL."' 
---
+++ assignment_2.py
@@ -0,0 +1,41 @@
+# Question 1
+
+first_name = 'Mr. Kyle Walker'
+second_name = 'Mrs. Irina Shayk'
+
+sliced_first_name = first_name[4:]
+print sliced_first_name
+
+sliced_second_name = second_name[5:]
+print sliced_second_name
+
+# Question 2
+
+current_nameplate = "Mr.Alan Walker"
+hero = "James Bond"
+end_replacement = hero[5:]
+start_replacement = current_nameplate[0:7]
+print start_replacement+end_replacement
+
+# Question 3
+
+runs = 68
+balls = 55
+target = 192
+print "Sachin scored %d runs in %d balls to set a target of %d." %(runs,balls,target)
+
+# Question 4
+
+brand_name = "Fast and Furious"
+sequel_number = 9
+
+movie_name = brand_name + str(sequel_number)
+
+print movie_name
+
+# Question 5
+
+vehicle = 'Honda City'
+mileage = 22
+unit = 'km/l'
+print 'My %s has a mileage of %d%s ' % (vehicle,mileage,unit) 
